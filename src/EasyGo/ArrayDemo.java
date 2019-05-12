0b3e9328cca05aa3e79dae70af26d6ebb4272523
sucre <1462339680@qq.com>
2019-05-12 11:29:16 +0800

    Array Demo

---
+++ src/EasyGo/ArrayDemo.java
@@ -0,0 +1,29 @@
+package EasyGo;
+
+public class ArrayDemo {
+    public static void main(String[] args){
+        double[] mylist = {1.9, 2.9, 3.4, 3.5};
+
+        // 打印所有数组元素
+        for(double element: mylist)
+        {
+            System.out.println(element);
+        }
+
+        double total = 0;
+        for(int i = 0; i < mylist.length; i++)
+        {
+            total += mylist[i];
+        }
+        System.out.println("Total:" + total);
+
+        double max = mylist[0];
+        for(int i = 1; i < mylist.length; i++)
+        {
+            if(mylist[i] > max) max = mylist[i];
+        }
+        System.out.println("Max:" + max);
+
+
+    }
+}
