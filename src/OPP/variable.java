343c5dab4c98d89d7d0fdcf61e047ba35b8f7218
sucre <1462339680@qq.com>
2019-05-21 18:41:33 +0800

    局部变量和成员变量重名时的就近原则

---
+++ src/OPP/variable.java
@@ -0,0 +1,18 @@
+package OPP;
+
+public class variable {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.name = "张三";
+        s.speak();
+    }
+}
+
+class Student{
+    String name;
+
+    public void speak(){
+        String name = "李四"; // 局部变量可以和成员变量重名，此时变量访问遵循就近原则
+        System.out.println("我的名字是：" + name);
+    }
+}
