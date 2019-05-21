2a6ea0a31de0ac9fbe0f74fc5202dd7a5afe0073
sucre <1462339680@qq.com>
2019-05-21 16:42:10 +0800

    Java Collection 注释

--- src/Collection/Muster.java
+++ src/Collection/Muster.java
@@ -1,6 +1,9 @@
 package Collection;
 import  java.util.*;
 
+/**
+ * Collection中包括List，Set，Map三类
+ */
 public class Muster {
     public static void main(String[] args) {
         Collection<String> list = new ArrayList<>();    //Collection为接口。不能实例化，使用ArrayList实例化
