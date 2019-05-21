753cb16eca192cc48657f1d89e93f1e34b726a59
sucre <1462339680@qq.com>
2019-05-21 18:53:24 +0800

    直接打印对象，输出为：包名.类名@地址

--- src/OPP/Dog.java
+++ src/OPP/Dog.java
@@ -15,12 +15,12 @@ public class Dog extends Mammal {
         Animal a = new Animal();
 //        Mammal m = new Mammal();
         Dog d = new Dog();
-
 //        System.out.println(m instanceof  Animal);
 //        System.out.println(d instanceof Mammal);
 //        System.out.println(d instanceof Animal);
-
         a.move();
         d.move();
+
+        System.out.println(d);  // 直接打印对象的结果是：包名.类名@地址
     }
 }
