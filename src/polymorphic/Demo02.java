883649442e2294a2a73229789d9530b7d9ef3bba
sucre <1462339680@qq.com>
2019-06-28 10:46:39 +0800

    多态访问成员方法

---
+++ src/polymorphic/Demo02.java
@@ -0,0 +1,32 @@
+package polymorphic;
+
+/**
+ * 多态访问成员方法：
+ * 编译看左边(父类) ，运行看右边(子类)
+ */
+
+public class Demo02 {
+    public static void main(String[] args) {
+
+        //父类引用指向子类对象[左边是父类，右边是子类]
+        Father2 son2 = new Son2();
+        son2.eat();
+    }
+}
+
+class Father2{
+
+    int num = 10;
+
+    public void eat(){
+        System.out.println("吃海鲜...");
+    }
+}
+
+class Son2 extends Father2{
+    int num = 20;
+
+    public void eat(){
+        System.out.println("吃野味...");
+    }
+}
