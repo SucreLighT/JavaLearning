d9d3f215c30c440c9a3227a915620bffccdcc6c2
sucre <1462339680@qq.com>
2019-07-15 14:14:53 +0800

    String类 等号与equals方法的比较

---
+++ src/stringDemo/Demo02.java
@@ -0,0 +1,15 @@
+package stringDemo;
+
+/**
+ * String类是可共享的常量，所以内容相同的String变量，内存地址相同
+ */
+public class Demo02 {
+    public static void main(String[] args) {
+        String s1 = "abc";
+        String s2 = "abc";
+
+        System.out.println(s1 ==s2);    //比较两个内存地址
+        System.out.println(s1.equals(s2));  //比较两个内容
+
+    }
+}
