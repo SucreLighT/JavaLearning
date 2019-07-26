cf43d9508b96df6dab42a017943c4ac9ee3c5f83
sucre <1462339680@qq.com>
2019-07-26 16:23:33 +0800

    集合类

---
+++ src/collectionDemo/Demo01.java
@@ -0,0 +1,22 @@
+package collectionDemo;
+
+/**
+ * @author sucre
+ * @date 2019-07-26
+ * @time 16:20
+ * @description 集合类
+ */
+
+/*
+    1.数组可以存储基本数据类型，也可以存储引用数据类型
+    集合只能存储引用数据类型（对象），也可以存储基本数据类型，但是会自动装箱
+
+    2.数组长度是固定的，集合长度可变
+    元素个数不固定时建议使用集合
+ */
+
+public class Demo01 {
+    public static void main(String[] args) {
+
+    }
+}
