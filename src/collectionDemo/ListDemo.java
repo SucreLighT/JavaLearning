52d87e475295c3446004c77f15655ecbd598deed
sucre <1462339680@qq.com>
2019-07-27 10:11:54 +0800

    集合类list迭代器ListIterator

--- src/collectionDemo/ListDemo.java
+++ src/collectionDemo/ListDemo.java
@@ -3,6 +3,7 @@ package collectionDemo;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.ListIterator;
 
 /**
  * @author sucre
@@ -15,6 +16,36 @@ public class ListDemo {
     public static void main(String[] args) {
 //        fun1();
 //        fun2();
+
+        fun();
+    }
+
+    /*	 ListIterator
+         boolean hasNext()是否有下一个
+         boolean hasPrevious()是否有前一个
+         Object next() 返回下一个元素
+         Object previous();返回上一个元素
+    */
+    private static void fun() {
+        List list = new ArrayList();
+        list.add("小胡");
+        list.add("小官");
+        list.add("小魏");
+        list.add("小刘");
+        list.add("小郭");
+
+        ListIterator iterator = list.listIterator();
+        while(iterator.hasNext()){
+            Object obj = iterator.next();
+            System.out.println(obj);
+        }
+
+        System.out.println("=========");
+        while (iterator.hasPrevious()) {//判断是否有上一个元素
+            //取出上一个元素
+            Object obj = iterator.previous();
+            System.out.println(obj);
+        }
     }
 
     // list集合的遍历
