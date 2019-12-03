bc33953ca9d536fa84f7709430f641872ee2e5c5
sucre <1462339680@qq.com>
2019-12-03 19:40:13 +0800

    Junit测试案例，并导入依赖包

---
+++ src/Java22_junitTest/HashMapTest1.java
@@ -0,0 +1,34 @@
+package Java22_junitTest;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author sucre
+ * @date 2019-12-03
+ * @time 10:37
+ * @description
+ */
+public class HashMapTest1 {
+    HashMapDemo hashMapDemo = new HashMapDemo();
+
+    @Before
+    public void setUp() throws Exception {
+        hashMapDemo.initialize();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        hashMapDemo.initialize();
+    }
+
+    @Test
+    /*
+     * 测试一：测试HashMap长度
+     */
+    public void test1(){
+        Assert.assertEquals(5,hashMapDemo.getSize());
+    }
+}
---
+++ src/Java22_junitTest/TestSuiteRunner.java
@@ -0,0 +1,20 @@
+package Java22_junitTest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * @author sucre
+ * @date 2019-12-03
+ * @time 16:58
+ * @description
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        HashMapTest1.class,
+        HashMapTest2.class,
+        HashMapTest3.class
+})
+public class TestSuiteRunner {
+}
